c527ae57f4490b123a43671f7fd450afce6458df
jrl1 <jrl1@bc88403a-3a91-4506-9056-abb4039e9f02>
2013-09-12 13:18:18 +0000

    Add favicon.ico and news.inc.php.

---
+++ website/news.inc.php
@@ -0,0 +1,19 @@
+<p><a href="https://sourceforge.net/p/wxlua/news/2013/09/wxlua-28123-released/">
+<strong>wxLua 2.8.12.3 Released</strong></a>
+<br /><small class="date">2012-09-12 08:45</small><br />
+wxLua 2.8.12.3 is a long overdue stable bugfix release.
+There are a few new wxWidgets classes and functions.
+The binding generator has been changed to accept simple valid C++ code.
+The precompiled versions are linked against the SVN trunk of the wxWidgets 2.8 branch (2.8.12 with bug fixes).
+Note that the wxLua source code can be compiled against wxWidgets 2.9.x too.
+<br /></p><p>
+
+<p><a href="https://sourceforge.net/p/wxlua/news/2012/07/wxlua-28122-released/">
+<strong>wxLua 2.8.12.2 Released</strong></a>
+<br /><small class="date">2012-07-26 20:37</small><br />
+wxLua 2.8.12.2 is a stable bugfix release that is a minor update to the internals of wxLua.
+The precompiled versions are linked against wxWidgets 2.8.12. Note that the source code can be compiled against wxWidgets 2.9.x.
+<br /></p><p>
+
+<a href="http://sourceforge.net/news/?group_id=140042">News archive &raquo;</a></p>
+<!-- <small><a href="http://sourceforge.net/news/?group_id=140042">Read More &raquo;</a></small> -->
