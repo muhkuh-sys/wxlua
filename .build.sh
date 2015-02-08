0d5a6d68b548de94961887a900a03955369d58cc
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-02-08 16:38:47 +0100

    Build wxWidgets and wxLua.

--- .build.sh
+++ .build.sh
@@ -34,8 +34,103 @@ if [ $STATUS -ne 0 ]; then
 	exit 1
 fi
 
-pushd /tmp/wxwidgets-3.0.2_win32/
-ls -l
+pushd /tmp/wxwidgets-3.0.2_win32/lib
+cp libwx_baseu-3.0-i686-w64-mingw32.a         libwx_baseu-3.0.a
+cp libwx_baseu_net-3.0-i686-w64-mingw32.a     libwx_baseu_net-3.0.a
+cp libwx_baseu_xml-3.0-i686-w64-mingw32.a     libwx_baseu_xml-3.0.a
+cp libwx_mswu_adv-3.0-i686-w64-mingw32.a      libwx_mswu_adv-3.0.a
+cp libwx_mswu_aui-3.0-i686-w64-mingw32.a      libwx_mswu_aui-3.0.a
+cp libwx_mswu_core-3.0-i686-w64-mingw32.a     libwx_mswu_core-3.0.a
+cp libwx_mswu_gl-3.0-i686-w64-mingw32.a       libwx_mswu_gl-3.0.a
+cp libwx_mswu_html-3.0-i686-w64-mingw32.a     libwx_mswu_html-3.0.a
+cp libwx_mswu_media-3.0-i686-w64-mingw32.a    libwx_mswu_media-3.0.a
+cp libwx_mswu_propgrid-3.0-i686-w64-mingw32.a libwx_mswu_propgrid-3.0.a
+cp libwx_mswu_qa-3.0-i686-w64-mingw32.a       libwx_mswu_qa-3.0.a
+cp libwx_mswu_ribbon-3.0-i686-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
+cp libwx_mswu_richtext-3.0-i686-w64-mingw32.a libwx_mswu_richtext-3.0.a
+cp libwx_mswu_stc-3.0-i686-w64-mingw32.a      libwx_mswu_stc-3.0.a
+cp libwx_mswu_webview-3.0-i686-w64-mingw32.a  libwx_mswu_webview-3.0.a
+cp libwx_mswu_xrc-3.0-i686-w64-mingw32.a      libwx_mswu_xrc-3.0.a
+cp libwxscintilla-3.0-i686-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
+popd
 
-#export LDFLAGS="-static -static-libgcc -static-libstdc++"
-#cmake -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxwidgets-3.0.2_win32/bin/wx-config -DwxWidgets_COMPONENTS="xrc;xml;net;propgrid;richtext;aui;stc;html;adv;core;base" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=x86 -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/i686-w64-mingw32-windres -DwxLua_LUA_LIBRARY_USE_BUILTIN=FALSE -DwxLua_LUA_LIBRARY_VERSION="5.1" -DLUA_INCLUDE_DIR=/home/baccy/workspace/muhkuh/lua/lib/lua-5.1.4/src -DLUA_LIBRARY=/home/baccy/workspace/muhkuh/build_windows32/lua/lib/lua-5.1.4/liblua.dll.a ..
+
+#-----------------------------------------------------------------------------
+#
+# Build wxWidgets for windows amd64.
+#
+mkdir build_win64
+cd build_win64
+../configure --host=x86_64-w64-mingw32 --prefix=/tmp/wxwidgets-3.0.2_win64/ --enable-vendor=muhkuh --disable-shared
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make install
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+pushd /tmp/wxwidgets-3.0.2_win64/
+cp libwx_baseu-3.0-x86_64-w64-mingw32.a         libwx_baseu-3.0.a
+cp libwx_baseu_net-3.0-x86_64-w64-mingw32.a     libwx_baseu_net-3.0.a
+cp libwx_baseu_xml-3.0-x86_64-w64-mingw32.a     libwx_baseu_xml-3.0.a
+cp libwx_mswu_adv-3.0-x86_64-w64-mingw32.a      libwx_mswu_adv-3.0.a
+cp libwx_mswu_aui-3.0-x86_64-w64-mingw32.a      libwx_mswu_aui-3.0.a
+cp libwx_mswu_core-3.0-x86_64-w64-mingw32.a     libwx_mswu_core-3.0.a
+cp libwx_mswu_gl-3.0-x86_64-w64-mingw32.a       libwx_mswu_gl-3.0.a
+cp libwx_mswu_html-3.0-x86_64-w64-mingw32.a     libwx_mswu_html-3.0.a
+cp libwx_mswu_media-3.0-x86_64-w64-mingw32.a    libwx_mswu_media-3.0.a
+cp libwx_mswu_propgrid-3.0-x86_64-w64-mingw32.a libwx_mswu_propgrid-3.0.a
+cp libwx_mswu_qa-3.0-x86_64-w64-mingw32.a       libwx_mswu_qa-3.0.a
+cp libwx_mswu_ribbon-3.0-x86_64-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
+cp libwx_mswu_richtext-3.0-x86_64-w64-mingw32.a libwx_mswu_richtext-3.0.a
+cp libwx_mswu_stc-3.0-x86_64-w64-mingw32.a      libwx_mswu_stc-3.0.a
+cp libwx_mswu_webview-3.0-x86_64-w64-mingw32.a  libwx_mswu_webview-3.0.a
+cp libwx_mswu_xrc-3.0-x86_64-w64-mingw32.a      libwx_mswu_xrc-3.0.a
+cp libwxscintilla-3.0-x86_64-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
+popd
+
+cd ..
+
+
+#-----------------------------------------------------------------------------
+#
+# Build wxLua for windows x86.
+#
+
+cd wxLua
+mkdir build_win32
+cd build_win32
+
+LDFLAGS="-static -static-libgcc -static-libstdc++" cmake -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxwidgets-3.0.2_win32/bin/wx-config -DwxWidgets_COMPONENTS="xrc;xml;net;propgrid;richtext;aui;stc;html;adv;core;base" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/i686-w64-mingw32-windres -DwxLua_LUA_LIBRARY_USE_BUILTIN=FALSE -DwxLua_LUA_LIBRARY_VERSION="5.1" -DLUA_INCLUDE_DIR=${LUA_WIN32_INCLUDE_DIR} -DLUA_LIBRARY=${LUA_WIN32_LIBRARY} ..
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+
+#-----------------------------------------------------------------------------
+#
+# Build wxLua for windows x86.
+#
+
+cd wxLua
+mkdir build_win64
+cd build_win64
+
+LDFLAGS="-static -static-libgcc -static-libstdc++" cmake -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxwidgets-3.0.2_win64/bin/wx-config -DwxWidgets_COMPONENTS="xrc;xml;net;propgrid;richtext;aui;stc;html;adv;core;base" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres -DwxLua_LUA_LIBRARY_USE_BUILTIN=FALSE -DwxLua_LUA_LIBRARY_VERSION="5.1" -DLUA_INCLUDE_DIR=${LUA_WIN64_INCLUDE_DIR} -DLUA_LIBRARY=${LUA_WIN64_LIBRARY} ..
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+
