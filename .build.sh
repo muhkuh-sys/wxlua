46e8c81c7725e7d8053d7c53b057d5519be88d68
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-07-02 21:23:23 +0200

    Use the prebuild wxWidgets from OBS.

--- .build.sh
+++ .build.sh
@@ -1,172 +1,56 @@
 #! /bin/bash
+set -e
 
-tar fxj wxWidgets-3.0.2.tar.bz2
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
 
-cd wxWidgets-3.0.2
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
+WORKING_DIR=`pwd`
 
-#-----------------------------------------------------------------------------
+# Remember the initial unmodified path
+PATH_ORG=${PATH}
+
+# Use mingw-w64-mbs.
+export PATH=/opt/mingw-w64-i686/bin:/opt/mingw-w64-x86_64/bin:${PATH_ORG}
+
+
+##############################################################################
 #
-# Build wxWidgets for windows x86.
+# Unpack lua51.
 #
-mkdir -p build_win32
-cd build_win32
-../configure --host=i686-w64-mingw32 --prefix=/tmp/wxwidgets-3.0.2_win32/ --enable-vendor=muhkuh --disable-shared
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make -j `nproc`
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make install
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-pushd /tmp/wxwidgets-3.0.2_win32/lib
-cp libwx_baseu-3.0-i686-w64-mingw32.a         libwx_baseu-3.0.a
-cp libwx_baseu_net-3.0-i686-w64-mingw32.a     libwx_baseu_net-3.0.a
-cp libwx_baseu_xml-3.0-i686-w64-mingw32.a     libwx_baseu_xml-3.0.a
-cp libwx_mswu_adv-3.0-i686-w64-mingw32.a      libwx_mswu_adv-3.0.a
-cp libwx_mswu_aui-3.0-i686-w64-mingw32.a      libwx_mswu_aui-3.0.a
-cp libwx_mswu_core-3.0-i686-w64-mingw32.a     libwx_mswu_core-3.0.a
-cp libwx_mswu_gl-3.0-i686-w64-mingw32.a       libwx_mswu_gl-3.0.a
-cp libwx_mswu_html-3.0-i686-w64-mingw32.a     libwx_mswu_html-3.0.a
-cp libwx_mswu_media-3.0-i686-w64-mingw32.a    libwx_mswu_media-3.0.a
-cp libwx_mswu_propgrid-3.0-i686-w64-mingw32.a libwx_mswu_propgrid-3.0.a
-cp libwx_mswu_qa-3.0-i686-w64-mingw32.a       libwx_mswu_qa-3.0.a
-cp libwx_mswu_ribbon-3.0-i686-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
-cp libwx_mswu_richtext-3.0-i686-w64-mingw32.a libwx_mswu_richtext-3.0.a
-cp libwx_mswu_stc-3.0-i686-w64-mingw32.a      libwx_mswu_stc-3.0.a
-cp libwx_mswu_webview-3.0-i686-w64-mingw32.a  libwx_mswu_webview-3.0.a
-cp libwx_mswu_xrc-3.0-i686-w64-mingw32.a      libwx_mswu_xrc-3.0.a
-cp libwxscintilla-3.0-i686-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
-cp libwxscintilla-3.0-i686-w64-mingw32.a      libwx_mswu_scintilla-3.0-i686-w64-mingw32.a
+mkdir -p lua-5.1.3
+pushd lua-5.1.3
+LUA_PATH=`pwd`
+unzip ${WORKING_DIR}/lua51-5.1.3.zip
 popd
 
-cd ..
+export LUA_WIN32_INCLUDE_DIR=${LUA_PATH}/windows_x86/include
+export LUA_WIN32_LIBRARY=${LUA_PATH}/windows_x86/lib/liblua.dll.a
+export LUA_WIN64_INCLUDE_DIR=${LUA_PATH}/windows_amd64/include
+export LUA_WIN64_LIBRARY=${LUA_PATH}/windows_amd64/lib/liblua.dll.a
 
 
-#-----------------------------------------------------------------------------
-#
-# Build wxWidgets for windows amd64.
-#
-mkdir -p build_win64
-cd build_win64
-../configure --host=x86_64-w64-mingw32 --prefix=/tmp/wxwidgets-3.0.2_win64/ --enable-vendor=muhkuh --disable-shared
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	cat config.log
-	exit 1
-fi
-
-make -j `nproc`
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make install
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-pushd /tmp/wxwidgets-3.0.2_win64/lib
-cp libwx_baseu-3.0-x86_64-w64-mingw32.a         libwx_baseu-3.0.a
-cp libwx_baseu_net-3.0-x86_64-w64-mingw32.a     libwx_baseu_net-3.0.a
-cp libwx_baseu_xml-3.0-x86_64-w64-mingw32.a     libwx_baseu_xml-3.0.a
-cp libwx_mswu_adv-3.0-x86_64-w64-mingw32.a      libwx_mswu_adv-3.0.a
-cp libwx_mswu_aui-3.0-x86_64-w64-mingw32.a      libwx_mswu_aui-3.0.a
-cp libwx_mswu_core-3.0-x86_64-w64-mingw32.a     libwx_mswu_core-3.0.a
-cp libwx_mswu_gl-3.0-x86_64-w64-mingw32.a       libwx_mswu_gl-3.0.a
-cp libwx_mswu_html-3.0-x86_64-w64-mingw32.a     libwx_mswu_html-3.0.a
-cp libwx_mswu_media-3.0-x86_64-w64-mingw32.a    libwx_mswu_media-3.0.a
-cp libwx_mswu_propgrid-3.0-x86_64-w64-mingw32.a libwx_mswu_propgrid-3.0.a
-cp libwx_mswu_qa-3.0-x86_64-w64-mingw32.a       libwx_mswu_qa-3.0.a
-cp libwx_mswu_ribbon-3.0-x86_64-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
-cp libwx_mswu_richtext-3.0-x86_64-w64-mingw32.a libwx_mswu_richtext-3.0.a
-cp libwx_mswu_stc-3.0-x86_64-w64-mingw32.a      libwx_mswu_stc-3.0.a
-cp libwx_mswu_webview-3.0-x86_64-w64-mingw32.a  libwx_mswu_webview-3.0.a
-cp libwx_mswu_xrc-3.0-x86_64-w64-mingw32.a      libwx_mswu_xrc-3.0.a
-cp libwxscintilla-3.0-x86_64-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
-cp libwxscintilla-3.0-x86_64-w64-mingw32.a      libwx_mswu_scintilla-3.0-x86_64-w64-mingw32.a
-popd
-
-cd ../..
-
-
-#-----------------------------------------------------------------------------
+##############################################################################
 #
 # Build wxLua for windows x86.
 #
 
-cd wxLua
-mkdir -p build_win32
-cd build_win32
 
-LDFLAGS="-static -static-libgcc -static-libstdc++" cmake -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxwidgets-3.0.2_win32/bin/wx-config -DwxWidgets_COMPONENTS="xrc;xml;net;propgrid;richtext;aui;stc;html;adv;core;base" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/i686-w64-mingw32-windres -DwxLua_LUA_LIBRARY_USE_BUILTIN=FALSE -DwxLua_LUA_LIBRARY_VERSION="5.1" -DwxLua_LUA_INCLUDE_DIR=/tmp/lua-5.1.4/windows_x86/dev/include -DwxLua_LUA_LIBRARY=/tmp/lua-5.1.4/windows_x86/dev/lib/liblua.dll.a ..
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make -j `nproc`
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make install DESTDIR=/tmp/wxlua_win32
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-cd ..
+mkdir -p build/wxlua_win32
+pushd build/wxlua_win32
+LDFLAGS="-static -static-libgcc -static-libstdc++" cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-i686/bin/wx-config -DwxWidgets_COMPONENTS="xrc;xml;net;propgrid;richtext;aui;stc;html;adv;core;base" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=i686-w64-mingw32-windres -DwxLua_LUA_LIBRARY_USE_BUILTIN=FALSE -DwxLua_LUA_LIBRARY_VERSION="5.1" -DwxLua_LUA_INCLUDE_DIR=${LUA_WIN32_INCLUDE_DIR} -DwxLua_LUA_LIBRARY=${LUA_WIN32_LIBRARY} ${WORKING_DIR}/wxLua
+make
+popd
 
 
-#-----------------------------------------------------------------------------
+##############################################################################
 #
-# Build wxLua for windows x86.
+# Build wxLua for windows amd64.
 #
+mkdir -p build/wxlua_win64
+pushd build/wxlua_win64
+LDFLAGS="-static -static-libgcc -static-libstdc++" cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-x86_64/bin/wx-config -DwxWidgets_COMPONENTS="xrc;xml;net;propgrid;richtext;aui;stc;html;adv;core;base" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres -DwxLua_LUA_LIBRARY_USE_BUILTIN=FALSE -DwxLua_LUA_LIBRARY_VERSION="5.1" -DwxLua_LUA_INCLUDE_DIR=${LUA_WIN64_INCLUDE_DIR} -DwxLua_LUA_LIBRARY=${LUA_WIN64_LIBRARY} ${WORKING_DIR}/wxLua
+make
+popd
 
-mkdir -p build_win64
-cd build_win64
 
-LDFLAGS="-static -static-libgcc -static-libstdc++" cmake -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxwidgets-3.0.2_win64/bin/wx-config -DwxWidgets_COMPONENTS="xrc;xml;net;propgrid;richtext;aui;stc;html;adv;core;base" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres -DwxLua_LUA_LIBRARY_USE_BUILTIN=FALSE -DwxLua_LUA_LIBRARY_VERSION="5.1" -DwxLua_LUA_INCLUDE_DIR=/tmp/lua-5.1.4/windows_amd64/dev/include -DwxLua_LUA_LIBRARY=/tmp/lua-5.1.4/windows_amd64/dev/lib/liblua.dll.a ..
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make -j `nproc`
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make install DESTDIR=/tmp/wxlua_win64
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
+# Restore the original path.
+export PATH=${PATH_ORG}
 
